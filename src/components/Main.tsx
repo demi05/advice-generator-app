e2d7c2768b98a5ca86afc85cb1f744f8f474bb26
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2024-06-22 09:08:51 +0100

    added necessary styles

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,19 @@
-import { Box, Flex, Image, Spinner, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Spinner, Text, keyframes, useBreakpointValue } from "@chakra-ui/react";
 import diceIcon from "../assets/images/icon-dice.svg";
+import patternDividerDesktop from "../assets/images/pattern-divider-desktop.svg";
+import patternDividerMobile from "../assets/images/pattern-divider-mobile.svg";
 import axios from "axios";
 import { useState } from "react";
+
 const Main = () => {
+  const patternDividerSrc = useBreakpointValue({
+    base: patternDividerMobile,
+    sm: patternDividerMobile,
+    md: patternDividerDesktop,
+    lg: patternDividerDesktop,
+    xl: patternDividerDesktop
+  })
+
   type SlipType = {
     advice: string;
     id: number;
@@ -29,6 +40,11 @@ const Main = () => {
       });
   };
 
+  const glow = keyframes`
+  0% { filter: brightness(1); }
+  50% { filter: brightness(1.5); }
+  `
+
   return (
     <Flex
       flexDir={"column"}
@@ -36,17 +52,44 @@ const Main = () => {
       alignItems={"center"}
       minHeight={"100vh"}
       backgroundColor={"hsl(218, 23%, 16%)"}
+      p={["2em", "3em", "0", "0"]}
     >
-      <Box backgroundColor={"hsl(217, 19%, 38%"} color={"#fff"}>
-        {advicee.slip && <Text color={"hsl(150, 100%, 66%)"}>ADVICE #{advicee.slip.id}</Text>}
+      <Flex
+        flexDir={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        gap={"1.5em"}
+        padding={"2em"}
+        paddingBottom={"3em"}
+        borderRadius={"0.8rem"}
+        backgroundColor={"hsl(217, 19%, 24%)"}
+        pos={"relative"}
+      >
+        {advicee.slip && (
+          <Text color={"hsl(150, 100%, 66%)"}>ADVICE #{advicee.slip.id}</Text>
+        )}
 
-          {isLoading ? (
-            <Spinner color="#fff" />
-          ) : advicee ? (
-            advicee.slip && <Text color={"hsl(193, 38%, 86%)"}>"{advicee.slip.advice}"</Text>
-          ) : (
-            <div>{error}</div>
-          )}
+        {isLoading ? (
+          <Spinner color="#fff" fontSize={"2rem"} />
+        ) : advicee ? (
+          advicee.slip && (
+            <Text
+              color={"hsl(193, 38%, 86%)"}
+              fontSize={"2rem"}
+              textAlign={"center"} maxW={["400px", "500px", "600px", "600px"]}
+            >
+              "{advicee.slip.advice}"
+            </Text>
+          )
+        ) : (
+          <div>{error}</div>
+        )}
+
+        <Flex justifyContent={"center"} alignItems={"center"}>
+          <Box>
+            <Image src= {patternDividerSrc} w={"100%"} fontWeight={900} />
+          </Box>
+        </Flex>
 
         <Flex
           alignItems={"center"}
@@ -55,11 +98,17 @@ const Main = () => {
           backgroundColor={"hsl(150, 100%, 66%)"}
           padding={"0.5em"}
           borderRadius={"50%"}
-          width={"30%"}
+          width={"10%"}
+          cursor={"pointer"}
+          pos={"absolute"}
+          bottom={"-25px"}
+          height={"50px"}
+          transition="all 0.3s ease-in-out"
+          _hover={{animation: `${glow} 1s infinite`}}
         >
-          <Image src={diceIcon} />
+          <Image src={diceIcon} fontSize={"2rem"} />
         </Flex>
-      </Box>
+      </Flex>
     </Flex>
   );
 };
